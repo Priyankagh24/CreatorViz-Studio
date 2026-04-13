55f92191e5a8bd689700da5e2b3941eb8e2e4abe
Priyanka Ghosh <pr27ghosh@gmail.com>
2026-04-13 20:40:25 +0530

    Add Render deployment config

--- app.py
+++ app.py
@@ -19,4 +19,4 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True, port=200)
+    app.run(debug=False, host="0.0.0.0", port=5000)  # debug=False for production!
